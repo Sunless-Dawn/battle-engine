aa48f2d3806c8e880061d986e9a85c011aee3908
Ryan Lucchese <ryan@energi.team>
2023-03-27 07:01:42 -0600

    setting up actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+extern crate env_logger;
+
+use actix_web::{
+    middleware::Logger,
+    web::{self, Data},
+    App, HttpServer,
+};
+use log::info;
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+use sunless_dawn_character::{Character, Class, EyeColor, HairColor, Sex, SkinColor};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    info!(
+        "Starting {} version {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))? // TODO: add --bind and --port command line flags
+    .run()
+    .await
 }
