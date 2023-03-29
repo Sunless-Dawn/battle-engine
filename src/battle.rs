0b17082ccbca190be7f3a56561261f2051d96595
Ryan Lucchese <ryan@energi.team>
2023-03-29 09:53:16 -0600

    working on websocket routing

--- src/battle.rs
+++ src/battle.rs
@@ -1,27 +1,38 @@
 use actix::{Actor, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{HttpRequest, HttpResponse};
 use actix_web::web::Payload;
 use actix_web_actors::ws;
-use futures::StreamExt;
-use sunless_dawn_character::{Character, Class, EyeColor, HairColor, Sex, SkinColor};
+//use sunless_dawn_character::Character;
 
+/*
 pub struct Battle {
   pub id:u64,
   pub player1:Character,
   pub player2:Character,
 }
+*/
 
-struct Echo;
+struct PlayerWS;
 
-impl Actor for Echo {
+impl PlayerWS {
+    pub fn route_message(&mut self, message:&str, ctx: &mut <PlayerWS as Actor>::Context) {
+        match message {
+            "/list\n" => ctx.text("list of stuff"),
+            "/help\n" => ctx.text("help command"),
+            _ => ctx.text("unknown command"),
+        }
+    }
+}
+
+impl Actor for PlayerWS {
     type Context = ws::WebsocketContext<Self>;
 }
 
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Echo {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for PlayerWS {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => self.route_message(&text, ctx),
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             Ok(ws::Message::Close(reason)) => ctx.close(reason),
             _ => (),
@@ -29,9 +40,9 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Echo {
     }
 }
 
-pub async fn websocket_route(
+pub async fn player_ws_route(
     req: HttpRequest,
     stream: Payload,
 ) -> Result<HttpResponse, actix_web::Error> {
-    ws::start(Echo {}, &req, stream)
+    ws::start(PlayerWS {}, &req, stream)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use actix_web::{
     App, HttpServer,
 };
 use log::info;
-use serde::{Deserialize, Serialize};
+//use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
 use sunless_dawn_character::{Character, Class, EyeColor, HairColor, Sex, SkinColor};
@@ -24,11 +24,16 @@ async fn main() -> std::io::Result<()> {
         env!("CARGO_PKG_VERSION")
     );
 
+    // just suppressing unused import warnings
+    {
+        let _ = Data::new(Mutex::new(Character::new("Name", Class::Mercenary, Sex::Male, HairColor::Black, EyeColor::Brown, SkinColor::Dark)));
+    }
+
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .service(web::scope("/health").configure(health::config))
-            .route("/ws/", web::get().to(battle::websocket_route))
+            .route("/ws/", web::get().to(battle::player_ws_route))
     })
     .bind(("127.0.0.1", 8080))? // TODO: add --bind and --port command line flags
     .run()
